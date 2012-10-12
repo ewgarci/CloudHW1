c5eabf89568ebfb1a5e31c1e0d24bbb587a86533
peter <pbl2108@columbia.edu>
2012-10-12 16:39:18 -0400

    Commited Eddy's code that gets the CPU usage from CloudWatch

--- CloudAssignment1/src/Admin.java
+++ CloudAssignment1/src/Admin.java
@@ -47,6 +47,7 @@ import com.amazonaws.services.ec2.model.DescribeImagesResult;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.DescribeKeyPairsResult;
 import com.amazonaws.services.ec2.model.Image;
+import com.amazonaws.services.ec2.model.KeyPairInfo;
 import com.amazonaws.services.ec2.model.Placement;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.InstanceState;
@@ -55,6 +56,7 @@ import com.amazonaws.services.ec2.model.KeyPair;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.SecurityGroup;
 import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.ec2.model.StopInstancesRequest;
 import com.amazonaws.services.ec2.model.Tag;
@@ -72,8 +74,8 @@ import com.amazonaws.services.s3.model.S3Object;
 
 
 public class Admin {
-	
-		
+
+
 	public static void main(String[] args) throws Exception {
 
 		AWSCredentials credentials = new PropertiesCredentials(
@@ -81,13 +83,10 @@ public class Admin {
 		AmazonEC2 ec2 = new AmazonEC2Client(credentials);
 		AmazonS3Client s3  = new AmazonS3Client(credentials);
 
-		DateFormat dateFormat = new SimpleDateFormat("_MMddyy_HHmmss");
-		//get current date time with Date()
-		Date date = new Date();
+		AmazonCloudWatchClient cloudWatch = new AmazonCloudWatchClient(credentials);
 
-
-		String securityGroup = "WorkSecurity" + dateFormat.format(date);
-		String keyName = "my_key" + dateFormat.format(date);
+		String securityGroup = "WorkSecurity";
+		String keyName = "my_key";
 		//String keyPath = "c://";
 		String zone = "us-east-1a";
 		String imageId = "ami-76f0061f";
@@ -95,67 +94,67 @@ public class Admin {
 
 		createSecurityGroup(ec2, securityGroup);
 		createKey(keyName, ec2);
-		//createBucket(s3, bucketName, zone);
-		
+		createBucket(s3, bucketName, zone);
+
 
 		OnDemandAWS bob = new OnDemandAWS(keyName, securityGroup, zone, imageId, "bob-PC");
 		//OnDemandAWS alice = new OnDemandAWS(keyName, securityGroup, zone, imageId, "alice-PC");
 
 		//alice.createInstance();
-			
 
-		
-		
+
+
+
 		int days = 1;
 		int numberOfChecks = 0;
 		long pingCW = 30*1000;
 		while (true) {
-			
+
 			if (isStartOfDay(numberOfChecks)) {
 				numberOfChecks++;
 				System.out.println("DAY: " + days);				
-				
+
 				//Recreate all instances and start them up;
 				bob.createInstance();			
-				
+
 				//Sleep before starting up
 				Thread.sleep(10*1000);
-				
+
 				System.out.println("Start up");
 				bob.startUpOnDemandAWS();
 				System.out.println("Attach EBS");
 				bob.attachEBS();
 				//bob.attachS3(bucketName);
-				
+
 				//Sleep for 30sec before pinging CloudWatch for the first time
 				Thread.sleep(pingCW);
 				continue;
 			} else if (isEndOfDay(numberOfChecks)) {
 				numberOfChecks = 0;
 				days++;
-				
+
 				/*Terminate all instances*/
 
 				//Try to shut down the machine		
 				System.out.println("Detach EBS");
 				bob.detachEBS();
 				System.out.println("Snapshot");
-				
+
 				bob.saveSnapShot();		
 				//wait for the snapshot to be created and then shutdown the machine
 				while(!bob.getSnapShotState().equalsIgnoreCase("available")){
 					Thread.sleep(15*1000);
 				}
 				System.out.println("Snapshot created");
-				
+
 				System.out.println("Terminate");		
 				bob.shutDownOnDemandAWS();
-				
+
 				/*Terminate all instances*/
 
 				if (days > 2)
 					break;
-				
+
 				System.out.println("SLEEP");				
 				//Sleep for the night - 2min
 				Thread.sleep(30 * 1000);				
@@ -164,128 +163,190 @@ public class Admin {
 
 			//See which machine is idle and try to shut it down.
 			numberOfChecks++;
-			
+
 			System.out.println("Check: " + numberOfChecks);
-			
+
 			//Sleep for 30sec before pinging CloudWatch again
 			Thread.sleep(pingCW);
 		}
 	}
-	
+
 	private static Boolean isStartOfDay(int numberOfChecks){
 		return numberOfChecks == 0;
 	}
-	
+
 	private static Boolean isEndOfDay(int numberOfChecks){
 		return numberOfChecks > 3;
 	}
-	
-	
-	
+
+
+
 	private static void createKey(String keyName, AmazonEC2 ec2){
 		try {
-		    CreateKeyPairRequest newKeyRequest = new CreateKeyPairRequest();
-		    newKeyRequest.setKeyName(keyName);
-		    CreateKeyPairResult keyresult = ec2.createKeyPair(newKeyRequest);
-		    //KeyPair keyPair = new KeyPair();
-		    //keyPair = keyresult.getKeyPair();
-		    //String privateKey = keyPair.getKeyMaterial();
-		    //writeKeytoFile(keyPath, privateKey);
-		    
+			List<KeyPairInfo> keyPairList = ec2.describeKeyPairs().getKeyPairs();
+			for (KeyPairInfo keyPair : keyPairList){
+				System.out.println(keyPair.getKeyName());
+				if ( keyName.equalsIgnoreCase(keyPair.getKeyName()) ){
+					return;
+				}
+			}
+			CreateKeyPairRequest newKeyRequest = new CreateKeyPairRequest();
+			newKeyRequest.setKeyName(keyName);
+			CreateKeyPairResult keyresult = ec2.createKeyPair(newKeyRequest);
+			//KeyPair keyPair = new KeyPair();
+			//keyPair = keyresult.getKeyPair();
+			//String privateKey = keyPair.getKeyMaterial();
+			//writeKeytoFile(keyPath, privateKey);
+
 		} catch (AmazonServiceException ase) {
-            System.out.println("Caught Exception: " + ase.getMessage());
-            System.out.println("Reponse Status Code: " + ase.getStatusCode());
-            System.out.println("Error Code: " + ase.getErrorCode());
-            System.out.println("Request ID: " + ase.getRequestId());
+			System.out.println("Caught Exception: " + ase.getMessage());
+			System.out.println("Reponse Status Code: " + ase.getStatusCode());
+			System.out.println("Error Code: " + ase.getErrorCode());
+			System.out.println("Request ID: " + ase.getRequestId());
+
 		}
 	}
-	
+
+
+	public static double getCPUUsage(AmazonCloudWatchClient cloudWatch, String instanceId){	
+		//create request message
+		GetMetricStatisticsRequest statRequest = new GetMetricStatisticsRequest();
+		//set up request message
+		statRequest.setNamespace("AWS/EC2"); //namespace
+		statRequest.setPeriod(60); //period of data
+		ArrayList<String> stats = new ArrayList<String>();
+		//Use one of these strings: Average, Maximum, Minimum, SampleCount, Sum 
+		stats.add("Average"); 
+		stats.add("Sum");
+		stats.add("Maximum");
+		statRequest.setStatistics(stats);
+		//Use one of these strings: CPUUtilization, NetworkIn, NetworkOut, DiskReadBytes, DiskWriteBytes, DiskReadOperations  
+		statRequest.setMetricName("CPUUtilization"); 
+		// set time
+		GregorianCalendar calendar = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+		calendar.add(GregorianCalendar.SECOND, -1 * calendar.get(GregorianCalendar.SECOND)); // 1 second ago
+		Date endTime = calendar.getTime();
+		calendar.add(GregorianCalendar.MINUTE, -1); // 1 minutes ago
+		Date startTime = calendar.getTime();
+		statRequest.setStartTime(startTime);
+		statRequest.setEndTime(endTime);
+
+		//specify an instance
+		ArrayList<Dimension> dimensions = new ArrayList<Dimension>();
+		dimensions.add(new Dimension().withName("InstanceId").withValue(instanceId));
+		statRequest.setDimensions(dimensions);
+
+		//get statistics
+		GetMetricStatisticsResult statResult = cloudWatch.getMetricStatistics(statRequest);
+
+		//display
+		System.out.println(statResult.toString());
+		List<Datapoint> dataList = statResult.getDatapoints();
+		Double averageCPU = null;
+		Date timeStamp = null;
+		for (Datapoint data : dataList){
+			averageCPU = data.getAverage();
+			timeStamp = data.getTimestamp();
+			System.out.println("Average CPU utlilization for last 1 minute since " +timeStamp.toString() + ": " +averageCPU);
+			System.out.println("Max CPU utlilization for last 1 minute since "+timeStamp.toString() + ": " + data.getMaximum());
+			System.out.println("Total CPU utlilization for last 1 minutes since "+timeStamp.toString() + ": " + data.getSum());
+		}
+		if (averageCPU == null)
+			return 0;
+		else 
+			return averageCPU;
+
+	}
+
+
 	private void writeKeytoFile(String path, String privateKey) {
-	
+
 	}
-	
-	// Setup the CloudWatch client and setup the metrics that we want to get
-	public void setupCloudWatch() {
-	}
-	
-	public void getCPUUsage(String instanceId){	
-	}
-	
+
 	public static void createBucket(AmazonS3Client s3, String bucketName, String zone){
 		try{
-		
-		List <Bucket> bucketList = s3.listBuckets();
 
-        for (Bucket bucket : bucketList){
-        	System.out.println(bucket.getName());
-        	if ( bucketName == bucket.getName() ){
-        		return;
-        	}
-        }
-        
-        s3.createBucket(bucketName);
-        		
-        return;	
-        	
+			List <Bucket> bucketList = s3.listBuckets();
+
+			for (Bucket bucket : bucketList){
+				System.out.println(bucket.getName());
+				if ( bucketName == bucket.getName() ){
+					return;
+				}
+			}
+
+			s3.createBucket(bucketName);
+
+			return;	
+
 		}catch (AmazonServiceException ase) {
-            System.out.println("Caught Exception: " + ase.getMessage());
-            System.out.println("Reponse Status Code: " + ase.getStatusCode());
-            System.out.println("Error Code: " + ase.getErrorCode());
-            System.out.println("Request ID: " + ase.getRequestId());
+			System.out.println("Caught Exception: " + ase.getMessage());
+			System.out.println("Reponse Status Code: " + ase.getStatusCode());
+			System.out.println("Error Code: " + ase.getErrorCode());
+			System.out.println("Request ID: " + ase.getRequestId());
 		}
 	}
-	
+
 	public static void createSecurityGroup(AmazonEC2 ec2, String securityGroup){
-		
+
+
+		List <SecurityGroup> secGroupList = ec2.describeSecurityGroups().getSecurityGroups();
+		for (SecurityGroup secGroup : secGroupList){
+			System.out.println(secGroup.getGroupName());
+			if ( securityGroup.equalsIgnoreCase(secGroup.getGroupName()) ){
+				return;
+			}
+		}
+
 		CreateSecurityGroupRequest createSecurityGroupRequest = 
 				new CreateSecurityGroupRequest();
-		
-		
-		
+
+
+
 		createSecurityGroupRequest.withGroupName(securityGroup)
-				.withDescription("My Java Security Group");
-		    	
-		    CreateSecurityGroupResult createSecurityGroupResult = 
-		    		ec2.createSecurityGroup(createSecurityGroupRequest);
-		  
-		    
-		    //SSH
+		.withDescription("My Java Security Group");
+
+		CreateSecurityGroupResult createSecurityGroupResult = 
+				ec2.createSecurityGroup(createSecurityGroupRequest);
+
+
+		//SSH
 		IpPermission ipPermission1 = new IpPermission();
 		ipPermission1.withIpRanges("0.0.0.0/0")
-		            .withIpProtocol("tcp")
-		            .withFromPort(22)
-		            .withToPort(22);
+		.withIpProtocol("tcp")
+		.withFromPort(22)
+		.withToPort(22);
 		//http
 		IpPermission ipPermission2 = new IpPermission();
 		ipPermission2.withIpRanges("0.0.0.0/0")
-		            .withIpProtocol("tcp")
-		            .withFromPort(80)
-		            .withToPort(80);
+		.withIpProtocol("tcp")
+		.withFromPort(80)
+		.withToPort(80);
 		//https
 		IpPermission ipPermission3 = new IpPermission();
 		ipPermission3.withIpRanges("0.0.0.0/0")
-		            .withIpProtocol("tcp")
-		            .withFromPort(443)
-		            .withToPort(443);
+		.withIpProtocol("tcp")
+		.withFromPort(443)
+		.withToPort(443);
 		//tcp
 		IpPermission ipPermission4 = new IpPermission();
 		ipPermission4.withIpRanges("0.0.0.0/0")
-		            .withIpProtocol("tcp")
-		            .withFromPort(65535)
-		            .withToPort(65535);
-		
+		.withIpProtocol("tcp")
+		.withFromPort(65535)
+		.withToPort(65535);
+
 		List<IpPermission> permissions = new ArrayList<IpPermission>();
 		permissions.add(ipPermission1);
 		permissions.add(ipPermission2);
 		permissions.add(ipPermission3);
 		permissions.add(ipPermission4);
-		
+
 		AuthorizeSecurityGroupIngressRequest authorizeSecurityGroupIngressRequest =
 				new AuthorizeSecurityGroupIngressRequest();
-			    	
+
 		authorizeSecurityGroupIngressRequest.withGroupName(securityGroup)
-		                                    .withIpPermissions(permissions);
-			        
+		.withIpPermissions(permissions);
+
 		ec2.authorizeSecurityGroupIngress(authorizeSecurityGroupIngressRequest);
 	}
 }
